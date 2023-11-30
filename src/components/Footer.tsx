16e0631363a8dbe00f10cb6fe21c63c28f9e2ad8
Timothy Kwok <timothykwok95@gmail.com>
2023-11-30 16:38:51 +0800

    nav layout done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<>
+			<footer>
+				<h1>Reach out to us</h1>
+
+				<p>© 1995 Temple Name, made with love</p>
+				<div>
+					<img src='' alt='phone-icon' /> <p>+65 6839 7517</p>
+				</div>
+				<div>
+					<img src='' alt='facebook-icon' />
+					<img src='' alt='instagram-icon' />
+				</div>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+	return (
+		<div className='container'>
+			<nav className='nav-container'>
+				<div className='nav-icon-container'>
+					<img src='' alt='temple-icon' />
+				</div>
+				<div className='nav-link-container'>
+					<li>
+						<a href='#'>Home</a>
+					</li>
+					<li>
+						<a href='#'>About</a>
+					</li>
+					<li>
+						<a href='#'>Contact Us</a>
+					</li>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
